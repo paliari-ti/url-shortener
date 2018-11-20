5287cf8c50c357c1e65595843f63a8d044b223f3
Daniel Fernando Lourusso <dflourusso@gmail.com>
2018-11-20 17:56:08 -0200

    use firestore as database

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = require('express')()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
-// Redis
-const redis = require('redis')
-const redisClient = redis.createClient({ host: 'redis', port: 6379 })
+// Firebase
+var admin = require('firebase-admin')
+var serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+})
+const collectionRef = admin.firestore().collection('links')
 
 const newId = () => {
   return Date.now().toString(36) + Math.floor(Math.random() * 999).toString(36)
@@ -17,25 +22,37 @@ app.get('/', (req, res) => {
 
 app.get('/:id', (req, res) => {
   const id = req.params['id']
-  redisClient.get(id, (error, url) => {
-    if (url) {
-      res.redirect(url)
-    } else {
+  collectionRef
+    .doc(id)
+    .get()
+    .then(doc => {
+      const data = doc.data()
+      res.redirect(data.url)
+    })
+    .catch(() => {
       res.send('Url not found')
-    }
-  })
+    })
 })
 
 app.post('/', (req, res) => {
+  const t1 = Date.now()
   if (req.body.token != process.env.API_TOKEN) {
     res.status(401).send('Unauthorized')
     return
   }
   const id = newId()
-  redisClient.set(id, req.body.url, redis.print)
-  res
-    .type('application/json')
-    .send({ id, url: `${process.env.API_ENDPOINT}/${id}` })
+  collectionRef
+    .doc(id)
+    .set({ url: req.body.url })
+    .then(() => {
+      const t2 = Date.now()
+      res
+        .type('application/json')
+        .send({ id, url: `${process.env.API_ENDPOINT}/${id}`, time: t2 - t1 })
+    })
+    .catch(e => {
+      response.send('Ops, something went wrong')
+    })
 })
 
 app.listen(3000, () => console.log('Server running'))
